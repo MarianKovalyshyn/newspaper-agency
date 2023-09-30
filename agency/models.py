0ff60b796d768fe5eea8bf450d9fbbba025889af
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-09-30 11:55:27 +0300

    Add str representation for models

--- agency/models.py
+++ agency/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Newspaper(models.Model):
@@ -15,4 +21,7 @@ class Newspaper(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField()
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-    publishers = models.ManyToManyField(Redactor)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self) -> str:
+        return self.title
